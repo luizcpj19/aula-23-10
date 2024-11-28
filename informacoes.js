3d87818fc512b56d939bf0120d5d2ebbd8758556
luizcpj19 <luizcpjs@gmail.com>
2024-11-28 09:04:43 -0300

    Update informacoes.js

--- informacoes.js
+++ informacoes.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoes() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o <span>3°C</span> tem <span>${quantidade_alunos} alunos</span> e apenas <span>${numero_votos} alunos</span> participaram do questionário? O professor <span>Victor Yanaguisawa</span> leciona <span>${numero_aula_Victor} aulas por semana</span>, e no colégio há <span>${numero_aula_Mazza} aulas por semana</span>. <br>Isso significa que <span>${porcentagem_Victor}%</span> das aulas acontece com um único professor. Abaixo está o resultado do Questionário`
+    paragrafo.innerHTML = `uma pesquisa mostra <span> o quanto em media</span> as pessoas<span> consomem frutas </span> em media <span> pela semana </span> participaram do questionário? O professor <span>Victor Yanaguisawa</span> leciona <span>${numero_aula_Victor} aulas por semana</span>, e no colégio há <span>${numero_aula_Mazza} aulas por semana</span>. <br>Isso significa que <span>${porcentagem_Victor}%</span> das aulas acontece com um único professor. Abaixo está o resultado do Questionário`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
